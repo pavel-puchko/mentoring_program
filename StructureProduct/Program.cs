cfb531da4eea9554879a8210a40c9a458988ed4c
Pavel Puchko <thefearofkitty@gmail.com>
2016-08-09 19:19:49 +0300

    product struct

---
+++ StructureProduct/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructureProduct
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		struct Product
+		{
+			public string Name { get; set; }
+
+			public float? Price { get; set; }
+
+			public int Count { get; set; }
+		}
+	}
+}

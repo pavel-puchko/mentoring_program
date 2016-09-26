bf68184dd0fb5f8936f5400f0af174c2edda4f71
Pavel Puchko <thefearofkitty@gmail.com>
2016-09-26 23:47:50 +0300

    Advanced C# (Types) 1

---
+++ Advanced C# (Types) 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace InfoDataOptimizitaion
+{
+	class Program
+	{
+	}
+
+	struct InfoData : IInfoData
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+
+	interface IInfoData
+	{
+		string FirstName { get; set; }
+		string LastName { get; set; }
+	}
+
+	class Source
+	{
+		internal void CheckAndProceed(List<InfoData> data)
+		{
+			var dest = new Destination();
+			var cData = data.Cast<IInfoData>().ToList();
+
+			dest.ProceedData(cData);
+		}
+	}
+
+	class Destination
+	{
+		internal void ProceedData(List<IInfoData> data)
+		{
+			foreach (var item in data)
+			{
+				//do something
+			}
+		}
+}
+}

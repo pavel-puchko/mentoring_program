da225f62289e7c591d9174b3f83e62803e0bb9de
Pavel Puchko <thefearofkitty@gmail.com>
2016-09-27 00:05:45 +0300

    BCL: Attribute

---
+++ BCLAttribute/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace BCLAttribute
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+		public class CustomRangeAttribute : ValidationAttribute
+		{
+			public decimal From;
+			public decimal To;
+
+			public CustomRangeAttribute(string from, string to)
+			{
+				From = decimal.Parse(from, CultureInfo.InvariantCulture);
+				To = decimal.Parse(to, CultureInfo.InvariantCulture);
+			}
+
+			public override bool IsValid(object value)
+			{
+				var decimalValue = (decimal)value;
+
+				return decimalValue >= From && decimalValue <= To;
+			}
+		}
+	}
+}

f7f9e62a24d6c07be7222083c6bb790c6e93ea25
pave-puchko <pavel_puchko@epam.com>
2016-09-27 13:58:32 +0300

    Reflection 1

---
+++ Reflection1/Reflection1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Reflection
+{
+	class Program
+	{
+		private static IList CreateDynamicList(Type type)
+		{
+			var list = Activator.CreateInstance(typeof(List<>).MakeGenericType(type));
+
+			return (IList)list;
+		}
+
+		static void Main(string[] args)
+		{
+			var list = CreateDynamicList(typeof(int));
+			for (var i = 0; i < 5; i++)
+			{
+				list.Add(i);
+
+			}
+		}
+	}
+}

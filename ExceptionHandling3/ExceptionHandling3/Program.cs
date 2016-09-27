c0b285af431e352c207f62d03486235a4cbc5932
pave-puchko <pavel_puchko@epam.com>
2016-09-27 13:48:38 +0300

    Exception handling 3

---
+++ ExceptionHandling3/ExceptionHandling3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExceptionHandling3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+
+	[Serializable]
+	public class StackNetworkException : Exception
+	{
+		private readonly int state;
+		public StackNetworkException()
+		{
+
+		}
+
+		public StackNetworkException(string message) : base(message)
+		{
+
+		}
+
+		public StackNetworkException(string message, int state) : base(message)
+		{
+			this.state = state;
+		}
+
+		public StackNetworkException(string message, Exception inner) : base(message, inner)
+		{
+
+		}
+
+		public StackNetworkException(string message, int state, Exception inner) : base(message, inner)
+		{
+			this.state = state;
+		}
+	}
+}

b0fbccc1655d2f37544714933113d4b7c01113b9
pave-puchko <pavel_puchko@epam.com>
2016-09-29 12:38:06 +0300

    Entity Framework code first from database

---
+++ EntityFramework/EntityFramework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var db = new Northwind())
+			{
+				var orders = (
+					from order in db.Orders
+					select order
+				).ToList();
+
+				Console.WriteLine("Press any key to exit...");
+				Console.ReadKey();
+			}
+		}
+	}
+}

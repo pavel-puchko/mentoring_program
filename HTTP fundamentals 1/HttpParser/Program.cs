03aaba6024eb092aeb21e2f064d9ce9b61cc1d68
pave-puchko <pavel_puchko@epam.com>
2016-11-14 22:24:16 +0300

    [+] http fundamentals

---
+++ HTTP fundamentals 1/HttpParser/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing.Imaging;
+
+
+
+namespace HttpParser
+{
+	class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new HttpParser();
+            parser.ParseUrl("https://code.google.com/archive/p/fizzler/", @"D:\site\", 2, ImageFormat.Png);
+            Console.ReadKey();
+        }
+    }
+}
